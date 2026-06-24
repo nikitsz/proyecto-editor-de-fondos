9b1d88d941ea38eaa057f0faf936d758543d9a83
nikitsz <ncalderon2026@alu.uct.cl>
2026-06-23 22:41:50 -0400

    Co-authored-by: LucsAndrs <LucsAndrs@users.noreply.github.com>

---
+++ funciones.py
@@ -0,0 +1,24 @@
+
+import os
+from datetime import datetime
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+
+#Devuelve la lista de los archivos dentro de la ruta
+os.listdir()
+
+#c:/Users/winxe/OneDrive/Escritorio/python/proyecto editor de imagenes/proyecto-editor-de-fondos
+
+#Para verificar si un archivo/carpeta existe
+#os.path.exists()
+
+#Para unir dos direcciones en una
+#os.path.join()
+
+#Para identificar si un archivo es jpg o png
+#os.path.splitext()
+
+
+#====== Definición de funciones ======
+#def cargar_imagen():
