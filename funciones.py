2020afc963a79035f8d1c39348639a6c8e184297
Lucas Villegas <lvillegas2024@alu.uct.cl>
2026-06-27 12:52:58 -0400

    Avance de colores de matrices

--- funciones.py
+++ funciones.py
@@ -52,14 +52,37 @@ print(carp_fondos)
 
 fondo = Image.open(carp_fondos)
 imagen = Image.open(carpeta)#prepara la imagen para mostrarla o algo asi nose :b es necesario guardarlo en una variable
-Timagen = imagen.size
+T_imagen = imagen.size
 print("el tamaño de la imagen es:", imagen.size, "por lo que el tamaño del fondo es:", imagen.size)#vemos el tamaño de la imagen (lo dice en la rubrica )
-fondo_ajustado = fondo.resize(Timagen)
+fondo_ajustado = fondo.resize(T_imagen)
 
-#fondo_ajustado.show()#muestra la imagen en la pántalla
+#fondo_ajustado.show()#muestra la imagen en la pantalla
 #imagen.show() #estos 2 nos servirian para mostrar la imagen pero primero tenemos que convertir las imagenes a matrices
 matriz_Obj = np.array(imagen)
 matriz_fon = np.array(fondo_ajustado)
 print(matriz_Obj.shape)
 print(matriz_fon.shape)
 
+#Para conocer los colore de un punto específico de la matriz
+#color_pixel = matriz_Obj[0, 0]
+#print("color del pixel: ", color_pixel)
+
+#0 = Rojo
+#1 = Verde
+#2 = Azul
+
+#canal_rojo = matriz_Obj[:, :, 0] #Los ":" se ocupan para seleccionar todas las filas y columnas de la matriz
+
+#Necesitaremos el np.copy para guardar la imagen original en una copia
+#matriz_mod = np.copy(matriz_Obj)
+
+#===== Acceder a colores de referencia ======#
+#Para las imagenes, generalmente el color del fondo liso es el mismo del primer pixel ([0, 0])
+#color_referencia = matriz_Obj[0, 0]
+#print("El color de referencia es: ", color_referencia)
+
+#===== Creación de la función de la nueva imagen =====#
+def nueva_imagen(matriz_Obj, matriz_fondo):
+    copia_matriz = np.copy(matriz_Obj)
+    color_referencia = matriz_Obj[0, 0]
+    mascara = matriz_Obj == color_referencia
