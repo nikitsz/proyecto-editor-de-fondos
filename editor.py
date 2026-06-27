7b07dfc45548371f867274fe68d509e74ab5d274
nikitsz <ncalderon2026@alu.uct.cl>
2026-06-27 01:27:35 -0400

    Co-authored-by: LucsAndrs <LucsAndrs@users.noreply.github.com>

--- editor.py
+++ editor.py
@@ -1,1 +1,22 @@
 #primer commit
+
+"""""
+#def cargar_imagen():
+    objetos = "Objetos/"
+    fondos = "Fondos/"
+    lista_archivos = os.listdir(fondos)
+    
+    return lista_archivos
+#print(cargar_imagen())"""
+
+"""
+def fondos(ruta):
+    if os.path.exists(ruta):
+        archivos = os.listdir(ruta)
+        lis = " "
+        for i in range(len(archivos)):
+            img = f"\n{i+1}. {archivos[i]}"
+            lis += img    
+        fon = f"Lista de fondos: \n {lis}"
+    return fon
+"""
--- funciones.py
+++ funciones.py
@@ -40,45 +40,26 @@ def carpeta_imagenes(ruta):
         print("La ruta no existe")
         return None
     return os.path.join(ruta, imagen)
-"""
-def fondos(ruta):
-    if os.path.exists(ruta):
-        archivos = os.listdir(ruta)
-        lis = " "
-        for i in range(len(archivos)):
-            img = f"\n{i+1}. {archivos[i]}"
-            lis += img    
-        fon = f"Lista de fondos: \n {lis}"
-    return fon
-"""
+
 
 
 #===== Llamar funciones =====#
 carpeta = carpeta_imagenes("Objetos/")
 print(carpeta)
 
-carp_fondos = ("Fondos/")
+carp_fondos = carpeta_imagenes("Fondos/")
 print(carp_fondos)
 
-fondos = carpeta_imagenes(carp_fondos)
-print(fondos)
-
-
+fondo = Image.open(carp_fondos)
 imagen = Image.open(carpeta)#prepara la imagen para mostrarla o algo asi nose :b es necesario guardarlo en una variable
-print("el tamaño de la imagen es:", imagen.size)#vemos el tamaño de la imagen (lo dice en la rubrica )
-imagen.show()#muestra la imagen en la pántalla
+Timagen = imagen.size
+print("el tamaño de la imagen es:", imagen.size, "por lo que el tamaño del fondo es:", imagen.size)#vemos el tamaño de la imagen (lo dice en la rubrica )
+fondo_ajustado = fondo.resize(Timagen)
 
-#########################################################
-#¡¡¡¡¡¡¡¡¡¡¡¡¡IMPORTANTE¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡
-#ningun cambio realizado aqui es definitivo solamente estaba probando codigos que nos podrian servir
-#y gemini me dijo que no era importante la interfaz pero yo lo quiero hacer con interfaz
-#elpepe67
-#########################################################
-"""""
-#def cargar_imagen():
-    objetos = "Objetos/"
-    fondos = "Fondos/"
-    lista_archivos = os.listdir(fondos)
-    
-    return lista_archivos
-#print(cargar_imagen())"""
+#fondo_ajustado.show()#muestra la imagen en la pántalla
+#imagen.show() #estos 2 nos servirian para mostrar la imagen pero primero tenemos que convertir las imagenes a matrices
+matriz_Obj = np.array(imagen)
+matriz_fon = np.array(fondo_ajustado)
+print(matriz_Obj.shape)
+print(matriz_fon.shape)
+
